d67e768dbc46722967792bd86f15f8b0c4c4f7b8
chiderafaith <faithjosephs114@gmail.com>
2022-11-05 07:46:51 +0100

    first commit

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -1,6 +1,84 @@
-exports.example = (req, res) => {
-    console.log("example")
-    res.send("Flight example")
+const {Flights} = require("../models/Flight");
+const {v4: uuid} = require("uuid");
+
+
+exports.getFlights = async (req, res) => {
+   try {
+    const flights = Flights;
+    res.status(200).json({
+        massage: "All Flights",
+        flights: flights,
+    });
+   }catch (err) {
+    res.status(500).json({massege: err});
+   }
 }
 
+exports.getFlight = async (req, res) => {
+    try {
+        let id = req.params.id;
+        const flight = Flights.find((flight) => flight.id === id);
+        res.status(200).json({
+            message: "flight found",
+            flight,
+        })
+    }catch (err) {
+        res.status(500).json({massege: err});
+    }
+}
 
+exports.addFlight = async (req, res) => {
+    try {
+      const {title, time, price, date} = await req.body;
+
+      const newFlight = {
+        id: uuid(),
+        title,
+        time,
+        price,
+        date,
+      };
+
+      Flights.push(newFlight);
+
+      res.status(201).json({
+        message: "Flight created",
+        newFlight,
+      });
+    }catch (err) {
+     res.status(500).json({massege: err});
+    }
+ }
+
+
+exports.updateFlight = async (req, res) => {
+    try {
+        let id = req.params.id;
+        const flight = Flights.find((flight) => flight.id === id);
+        const {title, time, price, date} = await req.body;
+        flight.title = title;
+        flight.time = time;
+        flight.price = price;
+        flight.date = date;
+        res.status(200).json({
+            message: "flight updated",
+            flight,
+        })
+    }catch (err) {
+        res.status(500).json({massege: err});
+    }
+}
+
+exports.deleteFlight = async (req, res) => {
+    try {
+        let id = req.params.id;
+        const flight = Flights.find((flight) => flight.id === id);
+        Flights.splice(Flights.indexOf(flight), 1);
+        res.status(200).json({
+            message: "flight deleted",
+            flight,
+        })
+    }catch (err) {
+        res.status(500).json({massege: err});
+    }
+}
--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -3,7 +3,12 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/flightController');
 
-router.get('/', controller.example)
+router
+ .get('/', controller.getFlights)
+ .get('/:id', controller.getFlight)
+ .post('/', controller.addFlight)
+ .put('/:id', controller.updateFlight)
+ .delete('/:id', controller.updateFlight);
 
 module.exports = router;
 
